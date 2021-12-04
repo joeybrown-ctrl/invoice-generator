f41d9e2b1d592aa4206f1750cdd7ae337271597c
George Joseph Brown <georgejosephbrown@Georges-iMac.local>
2021-12-04 09:53:37 -0700

    add service to routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const pdfService = require("../service/pdf-service");
 
 const router = express.Router();
 router.get("/invoice", (req, res, next) => {
-    
+    const dataStream = res.writeHead(200, {
+        "Content-Type": "application/pdf",
+        "Content-Disposition": "attachment;filename=invoice.pdf"
+    });
+
+    pdfService.generatePDF(
+        (chunk) => dataStream.write(chunk),
+        () => dataStream.end()
+    )
 });
 
 module.exports = router;
